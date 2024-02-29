afdec5036c1a5d07326a836f7b3acce2d796dfcc
eliyas41 <fentaeliyas@gmail.com>
2024-02-29 15:39:28 +0300

    use routing and finalize registration component

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,80 @@
+import React, { useRef } from 'react'
+import axios from '../axiosConfig'
+
+const Register = () => {
+
+  const userNameDom = useRef(null)
+  const firstNameDom = useRef(null)
+  const lastNameDom = useRef(null)
+  const emailDom = useRef(null)
+  const passwordDom = useRef(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // get values from the input fields
+    let usernameValue = userNameDom.current.value
+    let firstnameValue = firstNameDom.current.value
+    let lastnameValue = lastNameDom.current.value
+    let emailValue = emailDom.current.value
+    let passValue = passwordDom.current.value
+    if (
+      !usernameValue ||
+      !firstnameValue ||
+      !lastnameValue ||
+      !emailValue ||
+      !passValue
+    ) {
+      alert('Please provide all required information')
+      return;
+    }
+    try {
+      await axios.post('/users/register', {
+        username: userNameDom.current.value,
+        firstname: firstNameDom.current.value,
+        lastname: lastNameDom.current.value,
+        email: emailDom.current.value,
+        password: passwordDom.current.value
+      })
+      console.log('Registration Successful!')
+    } catch (err) {
+      console.log(err.message)
+      console.log("Username or Email already in use.")
+    }
+  }
+
+
+
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <span>Username: </span>
+          <input ref={userNameDom} type="text" placeholder='username' />
+        </div>
+        <br />
+        <div>
+          <span>First name: </span>
+          <input ref={firstNameDom} type="text" placeholder='first name' />
+        </div>
+        <br />
+        <div>
+          <span>Last name: </span>
+          <input ref={lastNameDom} type="text" placeholder='last name' />
+        </div>
+        <br />
+        <div>
+          <span>Email: </span>
+          <input ref={emailDom} type="email" placeholder='email' />
+        </div>
+        <br />
+        <div>
+          <span>Password: </span>
+          <input ref={passwordDom} type="password" placeholder='password' />
+        </div>
+        <button type='submit'>Register</button>
+      </form>
+    </section>
+  )
+}
+export default Register
